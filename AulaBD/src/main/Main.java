586a54092ad22e255f39d5b0136d262b5910e602
MateusPepice <mateuspepice@hotmail.com>
2023-10-18 19:10:03 -0300

    Arquivos alterados

---
+++ AulaBD/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import persistencia.DatabaseConnection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DatabaseConnection.getConnection();
+    }
+    
+}
---
+++ AulaBD/src/persistencia/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static Connection con = null;
+  
+    private static void criarConexao()
+    {
+        String url = "jdbc:mysql://localhost:3306/conexaobd";
+        String user = "root";
+        String pass = "univel";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Falha ao criar conexão com o banco de dados!\n"+e.getMessage());
+        }
+    }
+
+    public static Connection getConnection()
+    {
+        try{
+            if(con==null || con.isClosed()){
+                  criarConexao();
+            }
+            return con;
+        }catch(SQLException ex){
+            System.out.println("Falha ao obter conexão!\n"+ex.getMessage());
+            return null;
+        }
+    }
+}
